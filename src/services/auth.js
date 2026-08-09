cc7dce78e153d70b94e2d678ef842f20537ea3f3
Elena Polyakova <epolyk1970@gmail.com>
2026-08-09 19:27:53 +0300

    debug session auth log 1

--- src/services/auth.js
+++ src/services/auth.js
@@ -35,7 +35,7 @@ export const loginUser = async (payload) => {
     accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
     refreshTokenValidUntil: new Date(Date.now() + ONE_DAY),
   });
-
+  console.log("LOGIN CREATED SESSION:", newSession._id.toString());
   return newSession;
 };
 
